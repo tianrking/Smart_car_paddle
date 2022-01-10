8cbcd68ae28f6566e501a6c52d116eb53cd64e7c
Macyangxiaohu <571532316@qq.com>
2022-01-10 18:23:42 +0800

    xiugai_suojin

--- c++/demo/uart_recv.cpp
+++ c++/demo/uart_recv.cpp
@@ -7,8 +7,7 @@ std::shared_ptr<Driver> driver = nullptr;
 using namespace LibSerial;
 
 
-int main(int argc, char *argv[]) 
-{  
+int main(int argc, char *argv[]) {  
     int ret = 0;
 
      //与下文接收一个字节的数据并打印相关
@@ -17,31 +16,25 @@ int main(int argc, char *argv[])
 
     //预留串口的设备名为/dev/ttyPS1
     driver = std::make_shared<Driver>("/dev/ttyPS1", BaudRate::BAUD_115200);
-    if (driver == nullptr) 
-    {
+    if (driver == nullptr) {
     std::cout << "Create Driver Error ." << std::endl;
     return -1;
     }
     //串口初始化，打开串口设备及配置串口数据格式
     ret = driver->open();
-    if (ret != 0)
-    {
+    if (ret != 0){
     std::cout << "Driver Open failed ." << std::endl;
     return -1;
     }
 
-    while (1)
-    {
+    while (1){
         //循环阻塞接收一字节数据并打印
         ret = driver->recvdata(recv_data, timeout_ms);
-        if(ret == 0)
-        {
+        if(ret == 0){
             printf("The received data is %x\r\n",recv_data);
         }
-
     }
-    return 0;
-    
 
+    return 0;
 
 }
--- c++/include/core/uart_test.hpp
+++ c++/include/core/uart_test.hpp
@@ -5,10 +5,7 @@
 
 using namespace LibSerial;
 
-
-
-class Driver
-{
+class Driver{
 
 private:
     std::shared_ptr<SerialPort> _serial_port = nullptr;
@@ -17,42 +14,32 @@ private:
 
 private:
 
-    int recv(unsigned char &charBuffer, size_t msTimeout = 0) 
-    {
+    int recv(unsigned char &charBuffer, size_t msTimeout = 0) {
         /*try检测语句块有没有异常。如果没有发生异常,就检测不到。
         如果发生异常，則交给 catch 处理，执行 catch 中的语句* */
         try {
         /*从串口读取一个数据,指定msTimeout时长内,没有收到数据，抛出异常。
         如果msTimeout为0，则该方法将阻塞，直到数据可用为止。*/
         _serial_port->ReadByte(charBuffer, msTimeout);//可能出现异常的代码段
-        } 
-        catch (const ReadTimeout &)//catch捕获并处理 try 检测到的异常。
-        {
+        } catch (const ReadTimeout &){ //catch捕获并处理 try 检测到的异常。
             std::cerr << "The ReadByte() call has timed out." << std::endl;
             return -2;
-        } 
-        catch (const NotOpen &) //catch()中指明了当前 catch 可以处理的异常类型
-        {
+        } catch (const NotOpen &){ //catch()中指明了当前 catch 可以处理的异常类型
             std::cerr << "Port Not Open ..." << std::endl;
             return -1;
         }
         return 0;
     };
 
-    int send(unsigned char charbuffer)
-    {
+    int send(unsigned char charbuffer){
 
         //try检测语句块有没有异常
         try {
         _serial_port->WriteByte(charbuffer);//写数据到串口
-        } 
-        catch (const std::runtime_error &) //catch捕获并处理 try 检测到的异常。
-        {
+        } catch (const std::runtime_error &){ //catch捕获并处理 try 检测到的异常。
         std::cerr << "The Write() runtime_error." << std::endl;
         return -2;
-        } 
-        catch (const NotOpen &) //catch捕获并处理 try 检测到的异常。
-        {
+        } catch (const NotOpen &){ //catch捕获并处理 try 检测到的异常。
         std::cerr << "Port Not Open ..." << std::endl;
         return -1;
         }
@@ -82,21 +69,15 @@ public:
         _serial_port->SetFlowControl(FlowControl::FLOW_CONTROL_NONE);//设置流控
         _serial_port->SetParity(Parity::PARITY_NONE);//无校验
         _serial_port->SetStopBits(StopBits::STOP_BITS_1);//1个停止位
-        } 
-        catch (const OpenFailed &) //catch捕获并处理 try 检测到的异常。
-        {
+        } catch (const OpenFailed &){ //catch捕获并处理 try 检测到的异常。
         std::cerr << "Serial port: " << _port_name << "open failed ..."
                     << std::endl;
         return -2;
-        } 
-        catch (const AlreadyOpen &) //catch捕获并处理 try 检测到的异常。
-        {
+        } catch (const AlreadyOpen &){ //catch捕获并处理 try 检测到的异常。
         std::cerr << "Serial port: " << _port_name << "open failed ..."
                     << std::endl;
         return -3;
-        } 
-        catch (...) //catch捕获并处理 try 检测到的异常。
-        {
+        } catch (...){ //catch捕获并处理 try 检测到的异常。
         std::cerr << "Serial port: " << _port_name << " recv exception ..."
                     << std::endl;
         return -4;
@@ -109,16 +90,13 @@ public:
     int recvdata(unsigned char &charBuffer, size_t msTimeout){return recv(charBuffer,msTimeout);}
 
         
-    void close() 
-    {
+    void close() {
         if (_serial_port != nullptr) {
         /*关闭串口。串口的所有设置将会丢失，并且不能在串口上执行更多的I/O操作。*/
         _serial_port->Close();
         _serial_port = nullptr;
         }
     };
-
-
 
 
 
